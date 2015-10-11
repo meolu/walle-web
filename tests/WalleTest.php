ce599ccdbb1f2fa87af60730aa7ee7872fa2fba9
wushuiyong <wu.shuiyong@qq.com>
2015-10-11 22:47:50 +0800

    初步引入travis

---
+++ tests/WalleTest.php
@@ -0,0 +1,21 @@
+<?php
+/* *****************************************************************
+ * @Author: wushuiyong
+ * @Created Time : 日 10/11 22:45:02 2015
+ *
+ * @File Name: tests/WalleTest.php
+ * @Description:
+ * *****************************************************************/
+class WalleTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp() {
+    }
+
+    public function tearDown() {
+    }
+
+    public function testWalle() {
+        $this->assertEquals('walle-web', 'walle-web');
+    }
+
+}
