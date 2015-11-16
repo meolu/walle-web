a3dc82d124cfe3da09782d4aa7409969d4365eb8
wushuiyong <wu.shuiyong@qq.com>
2015-11-16 23:15:18 +0800

    translate into English

--- mail/confirmEmail.php
+++ mail/confirmEmail.php
@@ -8,10 +8,10 @@ use yii\helpers\Html;
 
 $confirmationLink = Yii::$app->urlManager->createAbsoluteUrl(['site/confirm-email', 'token' => $user->email_confirmation_token]);
 ?>
-亲爱的<strong><?= $user->realname ?></strong>:
+<?= yii::t('user', 'dear') ?><strong><?= $user->realname ?></strong>:
 
 <br><br>
-<span style="text-indent: 2em">点击以下链接完成注册，如不是本人操作，请忽略，谢谢！</span>
+<span style="text-indent: 2em"><?= yii::t('user', 'confirm email by click url') ?></span>
 
 <br><br>
-<h3><?= Html::a('确认激活', $confirmationLink) ?></h3>
+<h3><?= Html::a(yii::t('user', 'active'), $confirmationLink) ?></h3>
--- mail/passwordResetToken.php
+++ mail/passwordResetToken.php
@@ -8,11 +8,11 @@ use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
-亲爱的<strong><?= $user->realname ?></strong>:
+<?= yii::t('user', 'dear') ?><strong><?= $user->realname ?></strong>:
 
 <br><br>
-<span style="text-indent: 2em">点击以下链接完成重置密码，如不是本人操作，请忽略，谢谢！</span>
+<span style="text-indent: 2em"><?= yii::t('user', 'reset password by click url') ?></span>
 
 <br><br>
-<h3><?= Html::a('重置密码', $resetLink) ?></h3>
+<h3><?= Html::a(yii::t('user', 'reset password'), $resetLink) ?></h3>
 
