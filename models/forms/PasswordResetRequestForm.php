1d617972bac12133a0660940ca33a10eab77e40a
wushuiyong <wu.shuiyong@qq.com>
2015-11-26 16:28:31 +0800

    there is no need to reset password before active

--- models/forms/PasswordResetRequestForm.php
+++ models/forms/PasswordResetRequestForm.php
@@ -23,7 +23,7 @@ class PasswordResetRequestForm extends Model
             ['email', 'email'],
             ['email', 'exist',
                 'targetClass' => '\app\models\User',
-                'filter' => ['status' => User::STATUS_ACTIVE],
+//                'filter' => ['status' => User::STATUS_ACTIVE],
                 'message' => 'There is no user with such email.'
             ],
         ];
