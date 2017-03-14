8f87a86d026c421cd17711884360fbad89ca4a93
littlehz <littlehz@gmail.com>
2017-03-15 01:03:53 +0800

    去掉 setlocale，避免系统未安装 zh_CN language 时执行 shell 出现错误

--- config/local.php
+++ config/local.php
@@ -5,10 +5,6 @@ defined('YII_DEBUG') or define('YII_DEBUG', false);
 // Uncomment to enable dev environment. Recommended for development
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
-// zh_CN.UTF-8 => 中文,  en_US.UTF-8 => English
-setlocale(LC_ALL, 'zh_CN.UTF-8');
-putenv('LC_ALL=zh_CN.UTF-8');
-
 return [
     'components' => [
         'db' => [
