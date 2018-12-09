7e819e62b2101fe481b2d1f9dbc755f3b1208c96
walle <wushuiyong@walle-web.io>
2018-12-09 10:29:53 +0800

    walle 2.0 脚手架 -  若喜欢请不吝为我们项目点个star

--- walle/config/settings_dev.py
+++ walle/config/settings_dev.py
@@ -26,7 +26,7 @@ class DevConfig(Config):
     # 前端项目部署路径
     FE_PATH = os.path.abspath(Config.PROJECT_ROOT + '/../walle-fe/')
     AVATAR_PATH = 'avatar/'
-    UPLOAD_AVATAR = FE_PATH + 'dist/' + AVATAR_PATH
+    UPLOAD_AVATAR = FE_PATH + '/dist/' + AVATAR_PATH
 
     #email config
     MAIL_SERVER = 'smtp.exmail.qq.com'
