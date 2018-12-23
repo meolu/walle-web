0396031c9dc3ccfca67ee3ba9f497bd9d578096f
walle <wushuiyong@walle-web.io>
2018-12-23 09:10:37 +0800

    walle 2.0 alpha -  fix --exclude=

--- walle/service/deployer.py
+++ walle/service/deployer.py
@@ -203,7 +203,9 @@ class Deployer:
         # 压缩打包
         self.release_version_tar = '%s.tgz' % (self.release_version)
         with self.local.cd(self.local_codebase):
-            command = 'tar zcf %s %s' % (self.release_version_tar, self.release_version)
+            excludes = [i for i in self.project_info['excludes'].split('\n') if i.strip()]
+            excludes = ' --exclude='.join(excludes)
+            command = 'tar zcf  %s --exclude=%s %s' % (self.release_version_tar, excludes, self.release_version)
             result = self.local.run(command, wenv=self.config())
 
     def prev_release(self, waller):
