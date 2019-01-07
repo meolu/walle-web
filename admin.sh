adf4fc232c18cc0cc3f7f0c4f85733bd4a52dc8a
Alenx <alenx>
2019-01-07 18:00:07 +0800

    update docker-compose

--- admin.sh
+++ admin.sh
@@ -21,7 +21,7 @@ function init() {
         virtualenv --no-site-packages venv # 注意:安装失败请指定python路径. mac 可能会有用anaconda的python. 请不要mac试用, 麻烦多多
     fi
     echo "安装/更新可能缺少的依赖: mysql-community-devel gcc gcc-c++ python-devel"
-    sudo yum install -y mysql-community-devel gcc gcc-c++ python-devel
+    sudo yum install -y mysql-devel gcc gcc-c++ python-devel MySQL-python
     requirement
     echo "************************************************"
     echo -e "\033[32m init walle success \033[0m"
