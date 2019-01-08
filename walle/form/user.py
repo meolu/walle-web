1290d7ffb7435d3322475498da0dc0bf56f0b4a6
walle <wushuiyong@walle-web.io>
2019-01-08 18:37:27 +0800

    walle 2.0 alpha -  fix用户密码校验

--- walle/form/user.py
+++ walle/form/user.py
@@ -19,19 +19,19 @@ from wtforms.validators import Regexp
 
 validator_regx_password = "^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}"
 
-
 class UserForm(FlaskForm):
     email = StringField('email', [validators.email()])
-    password = PasswordField('Password', [validators.Length(min=6, max=35),
-                                          validators.Regexp(regex=validator_regx_password,
-                                                            message='密码至少6个字符，至少1个大写字母，1个小写字母，1个数字')])
-
+    password = PasswordField('Password', [])
     username = StringField('Username', [validators.Length(min=1, max=50)])
     role = StringField('role', [])
 
     def validate_email(self, field):
         if UserModel.query.filter_by(email=field.data).first():
             raise ValidationError('Email already register')
+
+    def validate_password(self, field):
+        if field.data and not re.match(validator_regx_password, field.data):
+            raise ValidationError('密码至少6个字符，至少1个大写字母，1个小写字母，1个数字')
 
     def form2dict(self):
         return {
@@ -48,9 +48,11 @@ class RegistrationForm(UserForm):
 
 class UserUpdateForm(FlaskForm):
     username = StringField('username', [])
-    password = PasswordField('Password', [validators.Length(min=6, max=35),
-                                          validators.Regexp(regex=validator_regx_password,
-                                                            message='密码至少6个字符，至少1个大写字母，1个小写字母，1个数字')])
+    password = PasswordField('Password', [])
+
+    def validate_password(self, field):
+        if field.data and not re.match(validator_regx_password, field.data):
+            raise ValidationError('密码至少6个字符，至少1个大写字母，1个小写字母，1个数字')
 
 
 class LoginForm(FlaskForm):
